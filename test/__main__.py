7e5cc77ca3ff95ffc7e4d02edce6dfb7a56d127f
Oscar Garzon <oscar.garzon@ciencias.unam.mx>
2022-06-16 01:59:36 -0500

    agregue __init__ y __main__

---
+++ test/__main__.py
@@ -0,0 +1,6 @@
+from test.test_main import Tests as tm
+from test.test_cifrar import Tests as tc
+from test.test_descifrar import Tests as td
+from test.test_utils import Tests as tu
+
+tm.correr_tests()
